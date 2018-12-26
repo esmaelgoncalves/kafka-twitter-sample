bd30f1ddd347e978c06622585246e211e8d8ef7d
Esmael Goncalves Pains <esmael.pains@certisign.com.br>
2018-12-26 19:05:13 -0200

    Initial Commit - twitter client and elasticsearch client

---
+++ kafka-consumer/src/main/java/br/com/egoncalves/kafka/sample/ElasticsearchClientKafkaConsumer.java
@@ -0,0 +1,95 @@
+package br.com.egoncalves.kafka.sample;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ElasticsearchClientKafkaConsumer {
+
+    public static Logger logger = LoggerFactory.getLogger(ElasticsearchClientKafkaConsumer.class);
+
+    public static RestHighLevelClient createClient(){
+
+        String hostname = "kafka-sample-elastic-9896799575.us-east-1.bonsaisearch.net";
+        String username = "efpjauxhc3";
+        String password = "ewhrvqsmvk";
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost(hostname, 443, "https"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+        return client;
+    }
+
+    public static KafkaConsumer createKafkaConsumer(String topic){
+        String bootstrapServers = "localhost:9092";
+        String groupId = "kafka-elasticsearch-app";
+
+        //create consumer properties
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        //create the consume;
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Arrays.asList(topic));
+        return consumer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient client = createClient();
+        KafkaConsumer<String, String> consumer = createKafkaConsumer("twitter_tweets");
+
+        //insert data into elasticsearch
+        while(true){
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord record: records) {
+                IndexRequest indexRequest = new IndexRequest("twitter", "tweets").source(record.value(), XContentType.JSON);
+                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                logger.info("_id: " + indexResponse.getId());
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        //client.close();
+    }
+}
---
+++ kafka-producer/src/main/java/br/com/egoncalves/kafka/sample/TwitterClientKafkaProducer.java
@@ -0,0 +1,138 @@
+package br.com.egoncalves.kafka.sample;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class TwitterClientKafkaProducer {
+    Logger logger = LoggerFactory.getLogger(TwitterClientKafkaProducer.class);
+
+
+    TwitterClientKafkaProducer() {
+    }
+
+    public static void main(String[] args) {
+        new TwitterClientKafkaProducer().run();
+    }
+
+    public void run() {
+        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+
+        //CREATE A TWITTER CLIENT
+        Client client = createTwitterClient(msgQueue);
+        // Attempts to establish a connection.
+        client.connect();
+
+        //CREATE A KAFKA PRODUCER
+        KafkaProducer<String, String> kafkaProducer = createKafkaProducer();
+
+        //ADD A SHUTDOWN HOOK
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+            logger.info("Stopping Application...");
+            logger.info("Shutting down client from twitter...");
+            client.stop();
+            logger.info("Closing the kafka producer...");
+            kafkaProducer.close();
+            logger.info("Done!");
+        }));
+
+        //LOOP INTO TWEETS
+        while(!client.isDone()){
+            String msg = null;
+            try {
+                 msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                client.stop();
+            }
+
+            if(msg != null){
+                logger.info(msg);
+                kafkaProducer.send(new ProducerRecord<>("twitter_tweets",null, msg), (recordMetadata, e) -> {
+
+                    if(e != null) {
+                        logger.error("Something going wrong.");
+                    }
+                });
+            }
+        }
+        logger.info("End of application!");
+    }
+
+    private KafkaProducer<String, String> createKafkaProducer() {
+        String bootstrapServers = "localhost:9092";
+
+        //create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //safe producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5"); //kafka 2.0 >= 1.1 can keep 5. Other cases use 1
+
+        //high throughput producer
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024)); //32KB batch size
+
+
+        //create the producer;
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        return producer;
+    }
+
+    public Client createTwitterClient(BlockingQueue<String> msgQueue) {
+        List<String> terms = Lists.newArrayList("bitcoin");
+
+        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+        // Optional: set up some followings and track terms
+
+        hosebirdEndpoint.trackTerms(terms);
+
+        // These secrets should be read from a config file
+        String consumerKey = "L8uH9hqDKEC1pLZHG4kmgplcC";
+        String consumerSecret = "S4Pz8uUP2o5osRXjAisCLoJkxIB1Z9pDbXxshz4iVpZ12XG5Je";
+        String token = "157143873-2TXPiEWRVLrgbiLUhYKHipgKI6PZUL2aHlMRuWuQ";
+        String tokenSecret = "7riYLTkLz0q3pFnDsZlZgybboSpR3O0BeRXAbw3OxI5Nu";
+
+        Authentication hosebirdAuth =
+                new OAuth1(consumerKey,
+                        consumerSecret,
+                        token,
+                        tokenSecret);
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+
+        Client hosebirdClient = builder.build();
+        return hosebirdClient;
+    }
+}
